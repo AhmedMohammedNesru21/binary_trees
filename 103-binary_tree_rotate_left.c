75dd362b2ec7377fa878523ea9653fb500e35a29
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-12-14 23:05:04 +0000

    Task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,64 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * count_nodes - Counts ths inside a tree
- * @root:  node
+ * binary_tree_rotate_left -  a left rotation
+ * @tree: Pointerroot
  *
- * Return: Number odes
+ * Return: Pointer  root
  */
-
-int count_nodes(binary_tree_t *root)
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	if (!root)
-		return (0);
-
-	return (1 + count_nodes(root->left) + count_nodes(root->right));
-}
-
-
-/**
- * is_complete - Checks if a tree is complete
- * @root: Pointer to tree's root
- * @index: Index of the node been evaluated
- * @n: number of trees nod
- *
- * Return: 1 if the tree is a heap, 0 otherwise
- */
-
-int is_complete(binary_tree_t *root, int index, int n)
-{
-	if (!root)
-		return (0);
-
-	if (index >= n)
-		return (0);
-	if (!root->left && !root->right)
-		return (1);
-	if (root->right && !root->left)
-		return (0);
-	if (root->left && !root->right)
-		return (is_complete(root->left, index * 2 + 1, n));
-
-	return (is_complete(root->left, index * 2 + 1, n) &&
-		is_complete(root->right, index * 2 + 2, n));
-}
-
-/**
- * binary_tree_is_complete - check for bt complete
- * @tree: Pointer to root
- *
- * Return: 1 if
- */
-int binary_tree_is_complete(const binary_tree_t *tree)
-{
-	int nod;
-	binary_tree_t *root;
+	binary_tree_t *chl, *parent = tree;
 
 	if (!tree)
-		return (0);
+		return (NULL);
 
-	root = (binary_tree_t *)tree;
-	nod = count_nodes(root);
+	chl = parent->right;
+	if (!chl)
+		return (tree);
 
-	return (is_complete(root, 0, nod));
+	if (chl->left)
+		chl->left->parent = parent;
+
+	parent->right = chl->left;
+	chl->left = parent;
+	chl->parent = parent->parent;
+	parent->parent = chl;
+
+	if (chl->parent && chl->parent->left == parent)
+		chl->parent->left = chl;
+	else if (chl->parent)
+		chl->parent->right = chl;
+
+	return (chl);
 }
